4f538750de884eb59e686548107678ec29a8772c
Aditya Maurya <128618775+AdityaaMauryaaa@users.noreply.github.com>
2024-10-09 19:33:27 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =144553
+let accountEmail="aditya@google.com"
+var accountPassword="Aditay"
+let accout;//undefined
+accoutnAddress="UP"// Not a good practice without any initializer consf val let....
+// accountId=2 // Not allowed as already initialised as constant
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail,accountId,accountPassword,accoutnAddress,accout])
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // use to treat all code as newer version of javascript
+
+//alert(3+3).    // we are using node.js so its not useful in browser it will give 6 but here error
+
+
+// console.log(3+3)    console.log(3+3).  //will give error
+console.log(4+4) //valid
+console.log('1'+4) ;  //valid
+
+
+let name="Aditay" //string datatype
+let age=19 // int datatype
+
+// number=? 2 to power 53
+// bigint
+// string ""
+// boolean true or false 1 or 0
+// null standalon value
+// undefined not initialised
+
+console.log(typeof(age)) // will give type of age
+console.log(typeof undefined)  // undefined is a type itself 
+console.log(typeof null)  // null is object datatype
+
