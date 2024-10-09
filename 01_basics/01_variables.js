85ac548add6e6ed9ec2df36d53b671c218a0abd9
Aditya Maurya <128618775+AdityaaMauryaaa@users.noreply.github.com>
2024-10-09 19:36:36 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId =144553
 let accountEmail="aditya@google.com"
 var accountPassword="Aditay"
-let accout;//undefined
+let accout;//undefined 
 accoutnAddress="UP"// Not a good practice without any initializer consf val let....
 // accountId=2 // Not allowed as already initialised as constant
 
