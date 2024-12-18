f7bb093d9c8a52668d9b187c4833f1e04766cd7c
Aditya Maurya <128618775+AdityaMauryaa@users.noreply.github.com>
2024-12-18 16:19:06 +0000

    included if else and switch cases

---
+++ 01_basics/08_Control_flow.js
@@ -0,0 +1,17 @@
+// Conditional statements
+
+        //  if(condition){...only condition is true...}
+        //  else if (condition_1) {....only if condition1 is true....}/// can be multiple else if
+        //  else {executes}
+
+// switch statements;
+    //pseudo code
+        // switch(expression){
+        //     case 1:
+        //         print;
+        //         break;
+        //     case 2:
+        //         print;
+        //         break;
+        // }
+
