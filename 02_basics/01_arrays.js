1a28dbf1193370e43ac2df89361f3c0cdfed12f7
Aditya Maurya <128618775+AdityaMauryaa@users.noreply.github.com>
2024-12-19 08:52:26 +0000

    removed error

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,7 @@
 // ++++++++++++++++++++++++++ ARRAY  +++++++++++++++++++++++++===
 
 
-const myArray=[2,4,56,3,5,8,09,5,5,8,0,"dfghjk"] // in javascript array can store myltiple types of datatypes
+const myArray=[2,4,56,3,5,8,9,5,5,8,0,"dfghjk"] // in javascript array can store myltiple types of datatypes
 
 const myNames=["Aditya ","MAurya","NandKishore","Nakul","Pratap","Maurya"];
 console.log(myArray.length) // function to get lentgh
@@ -12,9 +12,10 @@ console.log(myArray[4]) // function to get vale at index
 
 myArray.push(45)
 console.log(myArray)
-console.log(typeof myArray) // types will be object
+// console.log(typeof myArray) // types will be object
 
 myArray.push(567);
+console.log(myArray)
 myArray.pop();
 console.log(myArray)
 
@@ -30,7 +31,7 @@ const newArr=myArray.join() // join will make all elements from array to string
 console.log(newArr)
 console.log(typeof newArr)
 console.log("A",myArray)
-console.log(myArray.slice(2,6)). // print from 2 to 5th index element leaving 6th index
+console.log(myArray.slice(2,6)) // print from 2 to 5th index element leaving 6th index
 console.log("C" ,myArray)
 console.log("B" ,myArray)
 console.log(myArray.splice(2,6)) // print tille ranged and remove that range from orginal array
