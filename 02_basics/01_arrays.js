82653793dba4c4685d26e6218f814d62c17340fa
Aditya Maurya <128618775+AdityaMauryaa@users.noreply.github.com>
2024-12-19 09:09:53 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,5 @@
 // ++++++++++++++++++++++++++ ARRAY  +++++++++++++++++++++++++===
 
-
 const myArray=[2,4,56,3,5,8,9,5,5,8,0,"dfghjk"] // in javascript array can store myltiple types of datatypes
 
 const myNames=["Aditya ","MAurya","NandKishore","Nakul","Pratap","Maurya"];
@@ -19,20 +18,18 @@ console.log(myArray)
 myArray.pop();
 console.log(myArray)
 
-
 myArray.unshift(9) // this will insert the value in first index
 console.log(myArray)
 myArray.shift() // this will remove the value in first index
 console.log(myArray)
 
 console.log(myArray.includes(9)) // will result in boolean dataype whether the number exist or not
-const newArr=myArray.join() // join will make all elements from array to string
+const newArr=myArray.join("......") // join will make all elements from array to string
 
 console.log(newArr)
 console.log(typeof newArr)
 console.log("A",myArray)
 console.log(myArray.slice(2,6)) // print from 2 to 5th index element leaving 6th index
-console.log("C" ,myArray)
 console.log("B" ,myArray)
 console.log(myArray.splice(2,6)) // print tille ranged and remove that range from orginal array
 console.log("C" ,myArray)
