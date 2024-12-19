2bddc3b9366c400138c8f14fcb2eb64f1735d163
Aditya Maurya <128618775+AdityaMauryaa@users.noreply.github.com>
2024-12-19 08:42:05 +0000

    loops added

---
+++ 01_basics/09_loops.js
@@ -0,0 +1,25 @@
+// for loop
+//for(initalExpression; Condition; incrementExpression){ }
+                        // for (let i = 0; i < 9; i++) {   
+
+                        //     //nested loops:- use more loops inside another loop;
+                        //     for(let j=23;j<25;j++){
+                        //         console.log("Hello World",i,j);
+
+                        //     }
+                        // }
+
+// while loop
+// for while:- while(condition){.......____.......;increment/decrement;}
+                        // k=0
+                        // while(k<10){console.log(k);k++;
+                        // }
+
+
+                        
+// do while
+                        // do{
+                        //     ......something.....;
+                        //     inrecement/decrement;
+
+                        // }while(conidtion)
