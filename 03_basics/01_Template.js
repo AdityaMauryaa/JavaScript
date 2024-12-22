291311db3288945a60c467e7fa9bd8d9145fd760
Aditya Maurya <128618775+AdityaMauryaa@users.noreply.github.com>
2024-12-22 17:33:44 +0000

    Template

---
+++ 03_basics/01_Template.js
@@ -0,0 +1,31 @@
+// // Template Strings are also known as template literals,
+// //  are a feature of JAVASCRIPT to create stirngs with embedded expressions..
+// console.log(`This is random text i am adding lot of
+//                                spaces 
+                               
+                               
+                               
+                               
+//                                and backspaces as well
+//                                finally ending here`)
+
+
+
+
+// //These backticks helps in mainting and adding things in between text using,
+// //  ${..Anything inside can be a variable...}
+// const firstName="Aditya"
+// const lastName="Maurya"
+
+
+// console.log(`.      The full name is given by using literals 
+// ,where things have been replaced and final names comes out as,
+//              ${firstName} ${lastName}`);
+ 
+
+function info(){
+    console.log("Done");
+
+}
+
+console.log(`${info}`);
